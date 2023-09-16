8d8fad2ca27e8e4b2faaba2cdc502530f4277ca8
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2023-09-16 18:55:24 +0100

    Start code

---
+++ main.py
@@ -0,0 +1,29 @@
+from turtle import Turtle, Screen
+
+ali_the_turtle = Turtle()
+ali_the_turtle.shape("turtle")
+ali_the_turtle.color("red")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
